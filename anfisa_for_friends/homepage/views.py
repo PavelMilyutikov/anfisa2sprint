bb50afd1584a0a394105f67bd647b1d11d1fadef
Pavel Milyutikov <sevastopolrem@yandex.ru>
2026-04-13 23:19:36 +0300

    3

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 from ice_cream.models import IceCream
@@ -5,9 +6,15 @@ from ice_cream.models import IceCream
 
 def index(request):
     template = 'homepage/index.html'
+    # Запрашиваем нужные поля из базы данных:
     ice_cream_list = IceCream.objects.values(
-        'id', 'title', 'description').filter(
-            is_on_main=True).exclude(is_published=False)
+        'id', 'title', 'price', 'description'
+    ).filter(
+        # Проверяем, что
+        is_published=True,  # Сорт разрешён к публикации;
+        is_on_main=True,  # Сорт разрешён к публикации на главной странице;
+        category__is_published=True  # Категория разрешена к публикации.
+    )
     context = {'ice_cream_list': ice_cream_list, }
 
     return render(request, template, context)
--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -18,15 +18,17 @@ class IceCreamAdmin(admin.ModelAdmin):
     list_display = (
         'title',
         'description',
+        'price',
         'is_published',
         'is_on_main',
         'category',
-        'wrapper'
+        'wrapper',
     )
     list_editable = (
         'is_published',
         'is_on_main',
-        'category'
+        'category',
+        
     )
     search_fields = ('title',)
     list_filter = ('is_published',)
