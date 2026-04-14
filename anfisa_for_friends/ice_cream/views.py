686c645190b9b8d7c26b4a580a34f1c1246fc225
Pavel Milyutikov <sevastopolrem@yandex.ru>
2026-04-14 15:29:37 +0300

    4

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -6,9 +6,8 @@ from ice_cream.models import IceCream
 def ice_cream_detail(request, pk):
     template = 'ice_cream/detail.html'
     ice_cream = get_object_or_404(
-        IceCream.objects.values(
-            'title', 'description', 'id')
-        .filter(is_published=True), pk=pk
+        IceCream.objects.filter(
+            is_published=True, category__is_published=True), pk=pk
     )
     context = {'ice_cream': ice_cream}
     return render(request, template, context)
